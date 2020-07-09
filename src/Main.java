5f3e7fa077137c9ec062e7c0144a1fdc23511b77
tatsuya828828 <kkjy011894@gmail.com>
2020-07-09 23:17:41 +0900

    [finish]04-07

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,19 @@
 public class Main {
 	public static void main(String[] args) {
-		int[] array = {1, 2, 3};
-		// nullを代入することで参照を切ることができる
-		array = null;
-		// 参照が切られているためエラーが表示される
-		array[0] = 10;
+		// 2次元(多次元)配列
+		int[][] scores = new int[2][3];
+		scores[0][0] = 40;
+		scores[0][1] = 50;
+		scores[0][2] = 60;
+		scores[1][0] = 70;
+		scores[1][1] = 80;
+		scores[1][2] = 90;
+		System.out.println(scores[0][1]);
+
+		int[][] scores2 = {{40, 50, 60}, {70, 80, 90}};
+		System.out.println(scores.length);
+		System.out.println(scores[0].length);
+		System.out.println(scores[0][1]);
+		System.out.println(scores[1][2]);
 	}
 }
